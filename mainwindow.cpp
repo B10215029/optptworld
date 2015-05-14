69b26687badf5e3dd67be1fe7ca905609a4c5fcb
B10215029 <B10215029@mail.ntust.edu.tw>
2015-05-14 22:06:02 +0800

    修改按鈕名稱並設好事件

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,18 +2,48 @@
 #include "ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
+	QMainWindow(parent),
+	ui(new Ui::MainWindow)
 {
-    ui->setupUi(this);
+	ui->setupUi(this);
 }
 
 MainWindow::~MainWindow()
 {
-    delete ui;
+	delete ui;
 }
 
 void MainWindow::on_lineEdit_returnPressed()
 {
-    ui->textBrowser->append(ui->lineEdit->text());
+	ui->textBrowser->append(ui->lineEdit->text());
+}
+
+void MainWindow::on_pushButton_Golden_clicked()
+{
+
+}
+
+void MainWindow::on_pushButton_Powell_clicked()
+{
+
+}
+
+void MainWindow::on_pushButton_Newton_clicked()
+{
+
+}
+
+void MainWindow::on_pushButton_Quasi_clicked()
+{
+
+}
+
+void MainWindow::on_pushButton_Steep_clicked()
+{
+
+}
+
+void MainWindow::on_pushButton_Conjugate_clicked()
+{
+
 }
--- mainwindow.h
+++ mainwindow.h
@@ -9,17 +9,29 @@ class MainWindow;
 
 class MainWindow : public QMainWindow
 {
-    Q_OBJECT
+	Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = 0);
-    ~MainWindow();
+	explicit MainWindow(QWidget *parent = 0);
+	~MainWindow();
 
 private slots:
-    void on_lineEdit_returnPressed();
+	void on_lineEdit_returnPressed();
+
+	void on_pushButton_Golden_clicked();
+
+	void on_pushButton_Powell_clicked();
+
+	void on_pushButton_Newton_clicked();
+
+	void on_pushButton_Quasi_clicked();
+
+	void on_pushButton_Steep_clicked();
+
+	void on_pushButton_Conjugate_clicked();
 
 private:
-    Ui::MainWindow *ui;
+	Ui::MainWindow *ui;
 };
 
 #endif // MAINWINDOW_H
